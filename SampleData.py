f241bbc51b401ea03b9add02ed14ad2881d91ca2
ThomasProctor <proctortc@gmail.com>
2016-05-23 23:25:16 -0400

    Made code more easily customizable

--- SampleData.py
+++ SampleData.py
@@ -1,12 +1,19 @@
-
-# coding: utf-8
-
-# In[1]:
-
 import numpy as np
 import pandas as pd
 from math import ceil
 import pickle
+
+
+#ratio of data that you want to keep in sample:
+samplerate=0.3
+#If you want to sample the whole dataset, set just_booked=False, otherwise
+#it will just sample from the cases where is_booking==True
+just_booked=True
+#If you're having memory trouble, you can increase the number of chunks.
+nchunks=20
+
+sample_file_name='thirty_perc_booked_sample.csv'
+
 with open('datapath.txt') as f:
     datapath=f.readlines()[0].rstrip()
 
@@ -35,13 +42,9 @@ parser=lambda x: pd.to_datetime(x, format='%Y-%m-%d',errors='coerce')
 
 # In[9]:
 
-nchunks=20
+#nchunks=20
 chunksize=ceil(totaltrainrows/nchunks)
 
-
-# In[10]:
-
-samplerate=0.3
 
 
 # In[11]:
@@ -50,8 +53,8 @@ samplerate=0.3
 trainit=pd.read_csv(datapath+'train.csv',chunksize=chunksize,iterator=True,dtype=featuredtypes,parse_dates=dates,date_parser=parser)
 ichunk=0
 for chunk in trainit:
-    #Comment the line below to create a sample of the full dataset, not just those where is_booking==True
-    chunk=chunk[chunk['is_booking']==True]
+    if just_booked==True:
+		chunk=chunk[chunk['is_booking']==True]
     if ichunk==0:
         sample=chunk.sample(frac=samplerate)
     else:
@@ -67,5 +70,5 @@ print('sample shape='+str(sample.shape))
 
 # In[99]:
 
-sample.to_csv(datapath+'thirty_perc_booked_sample.csv')
+sample.to_csv(datapath+sample_file_name)
 
